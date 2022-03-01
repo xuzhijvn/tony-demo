9ca164b3a51e67deaed8e985778e55599bfae3ad
徐植君 <zhaowoganmane@gmail.com>
2022-03-01 16:33:19 +0800

    JVM类的卸载

---
+++ src/main/java/com/tony/clazz/unload/Sample.java
@@ -0,0 +1,13 @@
+/*
+*       Copyright© (2020).
+*/
+package com.tony.clazz.unload;
+
+/**
+* @author tony
+* @create 2022-03-01
+* @description:
+*/
+public class Sample {
+    private String name;
+}
