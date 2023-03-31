4657a841bd0fdad00c2f2fb8cae8c43865424c0e
xuzhijvn <xuzhijun256>
2023-03-31 10:57:25 +0800

    重载bean测试

---
+++ spring-cloud-core/src/main/java/com/example/springcloudcore/SpringCloudCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCoreApplication.class, args);
+    }
+
+}
