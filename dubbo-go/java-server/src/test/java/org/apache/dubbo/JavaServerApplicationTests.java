d7d2423c6b791b8ee7f2b1a5c8e5f799478ea682
xuzhijvn <xuzhijun256>
2024-05-05 11:48:46 +0800

    dubbo-go

---
+++ dubbo-go/java-server/src/test/java/org/apache/dubbo/JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
